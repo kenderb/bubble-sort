58d7bde4d9b711e88f9ae3d294893ccf48317f25
kenderb <kenderbolivar@gmail.com>
2020-10-20 16:27:55 -0500

    finish the bubble sort array

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -28,22 +28,24 @@ end
 p bubble_sort([8, 6, 9, 1, 21])
 
 
-def bubble_sort_by(block)
-  block_length = block.length
-  return block if block_length <= 1
+def bubble_sort_by(array)
+  array_length = array.length
+  return array if array_length <= 1
   swap = true
   while swap
     swap = false
-    (block_length - 1).times do |i|
-      sorted_block = yield(block[i], block[i + 1])
-      if sorted_block.positive?
-        block[i] , block[i + 1] = block[i + 1],block[i]
+    (array_length - 1).times do |i|
+      sorted_array = yield(array[i], array[i + 1])
+      if sorted_array.positive?
+        array[i] , array[i + 1] = array[i + 1],array[i]
+        swap = true
       end
     end
   end
 
-  return block
+  return array
 
 end
 
-p bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length } 
+p bubble_sort_by(["hello", "nope", "j"]) { |left, right| left.length - right.length } 
+
