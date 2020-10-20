0970f0413fc31aa9231b284bad977aa15a230ce5
kenderb <kenderbolivar@gmail.com>
2020-10-20 17:16:20 -0500

    delete white spaces

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -35,5 +35,4 @@ def bubble_sort_by(array)
   array
 end
 
-p bubble_sort_by(%w[hello nope j]) {|left, right| left.length - right.length} 
-
+p bubble_sort_by(%w[hello nope j]) { |left, right| left.length - right.length }
